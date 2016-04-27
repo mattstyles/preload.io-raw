9d312ef7e33d98c3f27e78ffc7996bd19ee01324
Matt Styles <matt@veryfizzyjelly.com>
2016-04-27 14:05:36 +0100

    #update example

--- examples/index.js
+++ examples/index.js
@@ -4,19 +4,19 @@ import 'whatwg-fetch'
 
 import Preloader from 'preload.io'
 import { EVENTS } from 'preload.io'
-import JSONLoader from '../lib'
+import RawLoader from '../lib'
 
 let preloader = new Preloader()
-preloader.register( new JSONLoader())
+preloader.register( new RawLoader() )
 
-preloader.load( './foo.json' )
+preloader.load( '/examples/foo.txt' )
 
 preloader.on( EVENTS.LOAD, event => {
   console.log( event )
 
   let code = document.createElement( 'code' )
   code.style[ 'white-space' ] = 'pre'
-  code.innerHTML = JSON.stringify( event.res, null, '  ' )
+  code.innerHTML = event.res
   document.body.appendChild( code )
 })
 preloader.on( EVENTS.LOAD_ERROR, event => {
